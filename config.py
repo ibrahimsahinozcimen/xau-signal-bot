dfbffddb45408d3aeb59578a93543efe1ff21285
ibrahimsahinozcimen <ibrahimsahinozcimennn@gmail.com>
2026-07-31 16:48:11 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# Telegram
+CHAT_ID = "1723318975"
+
+# İşlem yapılacak enstrüman
+SYMBOL = "XAUUSD"
+
+# Zaman dilimleri
+TIMEFRAME_TREND = "1h"
+TIMEFRAME_SIGNAL = "15m"
+TIMEFRAME_ENTRY = "5m"
+
+# Minimum sinyal puanı
+MIN_SCORE = 80
+
+# Haber filtresi (dakika)
+NEWS_FILTER_BEFORE = 30
+NEWS_FILTER_AFTER = 30
+
+# Risk / Ödül Oranı
+RISK_REWARD = 2
